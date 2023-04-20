6bb8af007cac78fdb4ee5b806df9db4e135143eb
YunaPark <jeannelvn@gmail.com>
2023-04-20 17:45:20 +0900

    unkown, void, never 추가

--- overview.ts
+++ overview.ts
@@ -57,3 +57,43 @@ const people: readonly [string, number, boolean] = ["nico", 12, false];
     any : 타입스트립트로부터 빠져나오고 싶을 때 쓰는 타입
     좋지 않지만 가끔 써야 할때가 있음
 */
+const people2: any[] = ["nico", 12, false];
+
+/* 
+    unknown : 변수의 타입을 미리 알지 못할 때 사용
+*/
+let aa: unknown;
+
+if (typeof aa === "number") {
+    let bb = aa + 1;
+}
+
+if (typeof aa === "string") {
+    let bb = aa.toUpperCase();
+}
+
+/* 
+    void : 아무것도 return하지 않는 함수를 대상으로 사용
+    비어있는 것(empty)
+*/
+function hello() {
+    console.log("x");
+}
+
+/* 
+    never : 함수가 절대 return하지 않을 때 발생
+    함수에서 예외가 발생할 때 리턴하지 않고 오류를 발생시킴
+*/
+function hello2(): never {
+    throw new Error("xxx");
+}
+
+function hello3(name: string | number) {
+    if (typeof name === "string") {
+        name; // string
+    } else if (typeof name === "number") {
+        name; // number
+    } else {
+        name; // never 이 코드는 절대 실행되지 않아야 한다
+    }
+}
